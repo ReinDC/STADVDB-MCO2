b7791bef9a3686885945c81fcbdbe752aca4de46
ReinDC <rein_delacruz@dlsu.edu.ph>
2024-12-02 21:29:42 +0800

    app id search

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -17,6 +17,7 @@ router.use((req, res, next) => {
 router.get('/', gameController.getFrontPage)
 router.get('/games', gameController.getGames);
 router.get('/search/games', gameController.searchGames);
+router.get('/search/appID', gameController.appIDSearch);
 
 router.post('/recover', gameController.recoverNode);
 
